54d09077ea53aec641c1df5b017fe21b582eeb93
Luma Bergmann <lumabergmann@gmail.com>
2026-06-29 23:51:33 -0300

    volta para a versão do commit a00a44fe5acc4f9b930085a1e48f02a5046ed155

--- agente_inteligente.py
+++ agente_inteligente.py
@@ -43,8 +43,7 @@ model = YOLO("best.pt")
 
 # Função para a criação de fala
 
-fila_frases = queue.Queue(maxsize=5)   # Fila que conterá as frases
-
+fila_frases = queue.Queue(maxsize = 5)   # Fila que conterá as frases
 
 def controlador_frases():    # Thread dedicada para fala
     while True:
@@ -67,9 +66,7 @@ def controlador_frases():    # Thread dedicada para fala
 
             if not voz_encontrada:
                 for voice in voices:
-                    # Checa o idioma ou o nome da voz
-                    lang = [str(l).upper()
-                            for l in voice.languages] if voice.languages else []
+                    lang = [str(l).upper() for l in voice.languages] if voice.languages else []    # Checa o idioma ou o nome da voz
                     if "PT" in lang or "BR" in lang or "PORTUGUESE" in voice.name.upper():
                         engine.setProperty('voice', voice.id)
                         voz_encontrada = True
@@ -83,15 +80,13 @@ def controlador_frases():    # Thread dedicada para fala
             engine.stop()
 
             del engine
-
+        
         except Exception as e:
             print("Erro ao reproduzir fala: ", e)
 
         fila_frases.task_done()
 
-
-# Inicializando a thread de frases
-thread_frases = threading.Thread(target=controlador_frases, daemon=True)
+thread_frases = threading.Thread(target = controlador_frases, daemon = True)     # Inicializando a thread de frases
 thread_frases.start()
 
 
@@ -100,60 +95,30 @@ thread_frases.start()
 root = tk.Tk()
 root.withdraw()
 
-dialog = tk.Toplevel(root)
-dialog.geometry('500x100')
-dialog.title("DETECÇÃO DE PLACAS DE TRÂNSITO BRASILEIRAS")
-dialog.resizable(False, False)
-dialog.grab_set()
-
-tk.Label(dialog, text='Selecione o tipo de entrada.', pady=10, padx=20).pack()
-
-btn_frame = tk.Frame(dialog, pady=10)
-btn_frame.pack()
-
-resposta = None
-
-
-def pick_value(value):
-    global dialog, resposta
-
-    resposta = value
-    dialog.destroy()
-
-
-tk.Button(btn_frame, text="🎬 Vídeo", width=10,
-          command=lambda: pick_value("video")).pack(side="left", padx=8)
-tk.Button(btn_frame, text="🖼️ Imagem", width=10,
-          command=lambda: pick_value("imagem")).pack(side="right", padx=8)
-
-dialog.protocol("WM_DELETE_WINDOW", lambda: pick_value(None))
-root.wait_window(dialog)
+resposta = messagebox.askyesnocancel("DETECÇÃO DE PLACAS DE TRÂNSITO BRASILEIRAS", "Clique em SIM para foto ou NÃO para vídeo")
 
 if resposta is None:
     print("Encerrando....")
     exit()
 
-if resposta == 'imagem':
+# SE A ESCOLHA FOI FOTO
+if resposta:
     formatos = [("Imagens", "*.jpg *.jpeg *.png *.bmp")]
-    caminho_foto = filedialog.askopenfilename(
-        title="Selecione a Foto", filetypes=formatos)
+    caminho_foto = filedialog.askopenfilename(title = "Selecione a Foto", filetypes = formatos)
 
     if not caminho_foto:
         print("Nenhum arquivo selecionado")
         exit()
 
     caminho_foto = os.path.abspath(caminho_foto)
-    results = model.predict(source=str(caminho_foto), conf=0.6)
+    results = model.predict(source = str(caminho_foto), conf = 0.6)
 
     for result in results:
         frame_placa = result.plot()
 
         cv2.namedWindow("Analisando foto", cv2.WINDOW_NORMAL)
 
-        h, w = frame_placa.shape
-        target_width = min(800, w)
-        target_height = int(h * target_width / w)
-        cv2.resizeWindow("Analisando foto", target_width, target_height)
+        cv2.resizeWindow("Analisando foto", 800, 600)
 
         cv2.imshow("Analisando foto", frame_placa)
 
@@ -163,7 +128,7 @@ if resposta == 'imagem':
 
         if len(caixas) == 0:
             print('NENHUMA PLACA DETECTADA')
-
+        
         for caixa in caixas:
             id_classe = int(caixa.cls[0])
             nome_classe = model.names[id_classe]
@@ -183,10 +148,10 @@ if resposta == 'imagem':
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
+# SE A ESCOLHA FOI VÍDEO
 else:
     formatos = [("Vídeos", "*.mp4 *.avi *.mkv *.mov")]
-    caminho_video = filedialog.askopenfilename(
-        title="Selecione o Vídeo", filetypes=formatos)
+    caminho_video = filedialog.askopenfilename(title = "Selecione o Vídeo", filetypes = formatos)
 
     if not caminho_video:
         print("Nenhum arquivo selecionado")
@@ -197,11 +162,7 @@ else:
 
     cv2.namedWindow("Analisando video", cv2.WINDOW_NORMAL)
 
-    h, w = captura.get(cv2.CAP_PROP_FRAME_HEIGHT), captura.get(
-        cv2.CAP_PROP_FRAME_WIDTH)
-    target_width = min(800, w)
-    target_height = int(h * target_width / w)
-    cv2.resizeWindow("Analisando video", target_width, target_height)
+    cv2.resizeWindow("Analisando video", 800, 600)
 
     placas_vistas = {}   # Armazena placas vistas recentemente para a ordem de fala
 
@@ -209,13 +170,13 @@ else:
 
     while captura.isOpened():
         possivel, frame_video = captura.read()
-
+        
         if not possivel:
             break
-
+        
         tempo_atual = time.time()
 
-        results = model.predict(source=frame_video, conf=0.6, stream=True)
+        results = model.predict(source = frame_video, conf = 0.6, stream = True)
 
         for result in results:
             frame_placa = result.plot()
@@ -242,7 +203,7 @@ else:
                 texto = ", ".join(frases_frame)
                 if not fila_frases.full():
                     fila_frases.put(texto)
-
+        
         cv2.imshow("Analisando video", frame_placa)
 
         if cv2.waitKey(1) & 0xFF == ord("q"):
